a83e513b75d326e3d755fcc84025b24a0b4a34a1
Guilherme <dosanjosguilherme88@gmail.com>
2026-04-17 17:17:14 -0300

    Inicializando requisicoes dependente.js

--- src/routes/dependente.js
+++ src/routes/dependente.js
@@ -4,4 +4,27 @@
     const dependente = express()
     dependente.use(express.json)
 
+    //buscar
+    dependente.get("", async function (req,res) {
+
+        const buscar = await prisma.dependente.findMany()
+
+        console.log("buscando dependentes.....")
+        res.json(buscar)
+        
+    })
+
+    //criar
+    dependente.post("", async function (req,res) {
+
+        const {nome,parentesco,dataNascimento,latitudeCentro,longitudeCentro,raioSeguranca,responsavelID} = req.body
+
+        if(!nome|!parentesco|!dataNascimento|!raioSeguranca|!responsavelID){
+            req.status(500).json({mensagem:"Voce deve preencher todos os itens"})
+        }
+        
+    })
+
+
+
     export default dependente;
