be90c480b4123e0e8bea5376d16e387b5506ad20
Guilherme <dosanjosguilherme88@gmail.com>
2026-04-27 17:34:33 -0300

    Atualizando dependente.js

--- src/routes/dependente.js
+++ src/routes/dependente.js
@@ -15,15 +15,84 @@
     })
 
     //criar
-    dependente.post("", async function (req,res) {
+    try{
+        dependente.post("", async function (req,res) {
 
         const {nome,parentesco,dataNascimento,latitudeCentro,longitudeCentro,raioSeguranca,responsavelID} = req.body
 
         if(!nome|!parentesco|!dataNascimento|!raioSeguranca|!responsavelID){
             req.status(500).json({mensagem:"Voce deve preencher todos os itens"})
         }
+
+        const cadastroDependente = prisma.dependente.create({
+            data:{
+                    nome,
+                    parentesco,
+                    dataNascimento,
+                    latitudeCentro,
+                    longitudeCentro,
+                    raioSeguranca,
+                    //Aqui é importantissimo que o dado que retorne do front seja o id do responsavel que esta faznedo o cadastro
+                    responsavelID:parseInt(responsavelID)
+                 },
+                 select:{
+                    nome:true,
+                    parentesco:true,
+                    dataNascimento:true
+
+                 }
+        })
+        res.status(201).json(cadastroDependente)
+
         
     })
+
+    }
+    catch(err){
+         res.status(500).json({ mensagem: "Ops! Algo deu errado. Tente novamente mais tarde." })
+    }
+
+    //buscar todos, apenas para verificação aqui no back
+    dependente.get("", async function(req,res){
+        const buscar = prisma.dependente.findMany
+
+        res.json(buscar)
+    })
+
+    //atualizar
+    dependente.put(":id", async function (req,res) {
+        const id = req.params
+        const {nome,parentesco,dataNascimento,latitudeCentro,longitudeCentro,raioSeguranca,responsavelID} = req.body
+
+        if(!id){
+             res.status(500).json({mensagem:"Ops! Id não identificado"})
+        }
+
+        try{
+            const atualizar = prisma.dependente.update({
+                where:{id:Number(id)},
+                data:{
+                     nome,
+                    parentesco,
+                    dataNascimento,
+                    latitudeCentro,
+                    longitudeCentro,
+                    raioSeguranca
+                }
+            })
+            res.json(atualizar)
+        }
+        catch(erro){
+            res.status(500).json({mensagem:"Erro inesperado, tente novamente ais tarde!"})
+
+        }
+
+
+        
+    })
+
+    //deletar
+    
 
 
 
